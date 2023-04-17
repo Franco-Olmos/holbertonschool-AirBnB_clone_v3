6ec2b686122119648ea124044bb1524772bfb201
IVanEDL <ivanaq3d@gmail.com>
2023-04-16 23:30:24 -0300

    Added imports

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ starts app """
 
-
+from os import getenv
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
